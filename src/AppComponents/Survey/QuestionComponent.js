58d6ecf2a2fbbdf734847c11f6e645a9fd202439
Roman <braid98@yandex.ru>
2020-05-03 16:21:38 +0300

    Covert Question and Survey to functional components

--- src/AppComponents/Survey/QuestionComponent.js
+++ src/AppComponents/Survey/QuestionComponent.js
@@ -1,16 +1,14 @@
 import React from "react";
 import Select from "./SelectComponent";
 
-class Question extends React.Component {
-  render() {
-    switch (this.props.question.type) {
-      case 'select':
-        return (
-          <Select question={this.props.question.label} answers={this.props.question.answers}/>
-        );
-      default:
-        return <div>yoooo</div>;
-    }
+function Question(props) {
+  switch (props.question.type) {
+    case 'select':
+      return (
+        <Select question={props.question.label} answers={props.question.answers}/>
+      );
+    default:
+      return <div>yoooo</div>;
   }
 }
 
--- src/AppComponents/Survey/SurveyComponent.js
+++ src/AppComponents/Survey/SurveyComponent.js
@@ -1,15 +1,13 @@
 import React from "react";
 import Question from "./QuestionComponent";
 
-class Survey extends React.Component {
-  render() {
-    const questions = this.props.questions.map(question => <Question question={question}/>);
-    return (
-      <div>
-        {questions}
-      </div>
-    );
-  }
+function Survey(props) {
+  const questions = props.questions.map(question => <Question question={question}/>);
+  return (
+    <div>
+      {questions}
+    </div>
+  );
 }
 
 export default Survey;
