5563f52ff4b2e9743fe49d2eb970cd65de557bc1
Roman <braid98@yandex.ru>
2020-05-03 15:14:15 +0300

    Update SelectComponent with material-ui

--- src/AppComponents/Survey/SelectComponent.js
+++ src/AppComponents/Survey/SelectComponent.js
@@ -1,17 +1,24 @@
 import React from 'react';
-
+import RadioGroup from "@material-ui/core/RadioGroup";
+import Radio from "@material-ui/core/Radio";
+import FormControl from "@material-ui/core/FormControl";
+import FormLabel from "@material-ui/core/FormLabel";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
 
 const Select = props => {
-  const options = props.answers.map(ans => <option key={ans.toString()} value={ans}>{ans}</option>);
+  // const options = props.answers.map(ans => <option key={ans.toString()} value={ans}>{ans}</option>);
+  const options = props.answers.map(ans => <FormControlLabel value={ans} control={<Radio />} label={ans}/>);
 
   return (
-    <div>
-      <label htmlFor={'id_select'}>{props.question}</label>
-      <select id={'id_select'}>
+    <FormControl>
+      <FormLabel>{props.question}</FormLabel>
+      <RadioGroup name={'radio'}>
         {options}
-      </select>
-    </div>
+      </RadioGroup>
+      {/*<label htmlFor={'id_select'}>{props.question}</label>*/}
+      {/*{options}*/}
+    </FormControl>
   );
-}
+};
 
 export default Select;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Select from "./AppComponents/Survey/SelectComponent";
+import Survey from "./AppComponents/Survey/SurveyComponent";
 
-const options = ['yoooo', 'boooo', 'kiya'];
+const questions = [
+  {
+    type: 'select',
+    label: 'Вопрос 1',
+    answers: ['ответ 1', 'ответ 2', 'ответ 3'],
+  },
+  {
+    type: 'select',
+    label: 'Вопрос 2',
+    answers: ['ответ 1', 'ответ 2', 'ответ 3'],
+  },
+  {
+    type: 'select',
+    label: 'Вопрос 3',
+    answers: ['ответ 1', 'ответ 2', 'ответ 3'],
+  },
+
+];
 
 ReactDOM.render(
-  <Select question={'Какой-то вопрос'} answers={options} />,
+  <Survey questions={questions} />,
   document.getElementById('root')
 );
