f18133c72b2fe901368e32e048d39c5d0a459f09
IvanetsAS <ivanetc.aleksandr@gmail.com>
2020-05-03 13:45:04 +0300

    Удалил мусор. Добавил тупой header

---
+++ src/AppComponents/HeaderComponent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../index.css'
+
+function Header() {
+    return (
+        <header className="App-header">
+            <div className="App-name"> Дневник пациента </div>
+        </header>
+    );
+}
+
+export default Header;
