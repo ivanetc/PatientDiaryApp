95549ccd7f418086d4290579e8887b6559d462fc
IvanetsAS <ivanetc.aleksandr@gmail.com>
2020-05-05 02:31:23 +0300

    header

--- src/AppComponents/HeaderComponent.js
+++ src/AppComponents/HeaderComponent.js
@@ -4,26 +4,27 @@ import {Link} from "react-router-dom";
 import logo from "../Resources/Images/logo.png"
 import Button from "@material-ui/core/Button";
 import Data from "../DataClasses/Data.js";
+import { useLocation } from 'react-router-dom'
 
 
 function Header() {
     let user = Data.Data.getUserProfile(0);
+    let location = useLocation();
+
     return (
         <header className="App-header">
             <img src={logo} alt="Логотип"/>
-            <div className="App-name"> Дневник пациента </div>
-            <div className="App-navi">
-                <Link className="App-navi-item" to='/profile'>Профиль </Link>
-                <Link className="App-navi-item" to='/daily_plan'>Дневной план </Link>
-                <Link className="App-navi-item" to='/recommendations'>Рекомендации</Link>
-                <Link className="App-navi-item" to='/calendar'>Календарь</Link>
-                <Link className="App-navi-item" to='/fill_survey'>Сообщения</Link>
-                <Link className="App-navi-item" to='/fill_survey'>Справка</Link>
+            <div className={"App-name"}> Дневник пациента </div>
+            <div className={"App-navi" + getBlockVisibilityClasses(location)}>
+                <Link className={getLinkClassesName(location, '/profile')} to='/profile'>Профиль </Link>
+                <Link className={getLinkClassesName(location, '/daily_plan')} to='/daily_plan'>Дневной план </Link>
+                <Link className={getLinkClassesName(location, '/recommendations')} to='/recommendations'>Рекомендации</Link>
+                <Link className={getLinkClassesName(location, '/calendar')} to='/calendar'>Календарь</Link>
             </div>
-            <div className="App-header-right-part">
+            <div className={"App-header-right-part" + getBlockVisibilityClasses(location)}>
                 <a> Добрый день, {user.firstName} {user.lastName} </a>
             </div>
-            <div className="App-header-exit-button">
+            <div className={"App-header-exit-button" + getBlockVisibilityClasses(location)}>
               <Link to='/'>
                 <Button size="small" variant="contained" color="secondary">
                     Выход
@@ -33,5 +34,19 @@ function Header() {
         </header>
     );
 }
+function getLinkClassesName(location, path) {
+    if (path === location.pathname)
+        return "App-navi-item App-navi-item-underlined";
+    else
+        return "App-navi-item";
+}
+
+function getBlockVisibilityClasses(location) {
+    if (location.pathname === "/")
+        return " App-navi-hided";
+    else
+        return "";
+}
+
 
 export default Header;
