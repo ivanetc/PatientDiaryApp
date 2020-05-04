b7a3ce0380ef6165ec246e94261135da5247c41b
IvanetsAS <ivanetc.aleksandr@gmail.com>
2020-05-04 22:02:39 +0300

    результаты опроса

--- src/AppComponents/DailyPlan/SurveyResultComponent.js
+++ src/AppComponents/DailyPlan/SurveyResultComponent.js
@@ -2,8 +2,10 @@ import React from "react";
 import '../../index.scss'
 import Paper from "@material-ui/core/Paper";
 import Survey from "../../DataClasses/survey";
+import Data from "../../DataClasses/Data"
 
 function SurveyResultComponent(props) {
+
     const surveyId = props.match.params.id;
     const AllSurveys = Survey.Survey;
     let currentSurvey = AllSurveys[surveyId];
@@ -12,6 +14,7 @@ function SurveyResultComponent(props) {
             <Paper className="all_profile_content">
                 <div className="profile_header">Опрос "{currentSurvey.name}" завершен</div>
                 <span className="survey_result_description">Результат опроса: {props.match.params.result}</span>
+                <p className="survey_result_description">{Data.Data.getSurveyDecoding(surveyId)}</p>
 
             </Paper>
         </div>;
